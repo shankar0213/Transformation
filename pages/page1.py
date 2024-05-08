bac738b9881d24b83fd89589f25ccf280e1cca6b
shankar0213 <165781085+shankar0213@users.noreply.github.com>
2024-05-08 15:02:31 +0530

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,17 +1,57 @@
 from navigation import make_sidebar
 import streamlit as st
+import pandas as pd
+from pytube import Playlist
+from streamlit_player import st_player
+import os
 
 make_sidebar()
 
-st.write(
-    """
-# 🔓 Secret Company Stuff
 
-This is a secret page that only logged-in users can see.
-
-Don't tell anyone.
-
-For real.
-
-"""
-)
+ 
+def get_playlist_videos(playlist_url):
+    playlist = Playlist(playlist_url)
+    return playlist.video_urls
+ 
+def main():
+    z=''
+    st.title("YouTube Playlist Player")
+ 
+    playlist_url = st.text_input("Enter YouTube Playlist Link:")
+    if playlist_url:
+        playlist_videos = get_playlist_videos(playlist_url)
+ 
+        if st.button("Play Playlist"):
+            for i in range(len(playlist_url)):
+                if playlist_url[i] == '=':
+                    z=(playlist_url[i+1:len(playlist_url)])
+                    break
+            print('z=',z)        
+         
+            data = {'sno':[1],
+           
+                'playlist': ['PL89t04T3yNybk1OM9npy_sqgGamrv843F&si=jxbmR9KqxeqElgAG'],
+                'name': ['name1']
+            }
+            data.update({"playlist": z})
+ 
+# Create a DataFrame from the dictionary
+            df = pd.DataFrame(data)
+            df=df.sample(frac=1).reset_index()
+           
+            x=0
+            html =  """
+            <iframe id="ytplayer" type="text/html" width="685" height="333"
+            src="https://www.youtube.com/embed/?listType=playlist&list={}"
+             encrypted-media; gyroscope; picture-in-picture"
+            frameborder="0" allowfullscreen allow="autoplay"
+            >
+            """
+         
+            while x<1:
+                st.components.v1.html(html.format(df.at[x,'playlist']),  height=350,scrolling=False)
+                x+=1
+ 
+ 
+if __name__ == "__main__":
+    main()
